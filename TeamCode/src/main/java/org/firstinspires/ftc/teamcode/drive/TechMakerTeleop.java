da762505f1a65b62c866ec886d8d16a7b624a1f0
TechMaker <122989919+techmaker1234@users.noreply.github.com>
2024-01-18 08:59:07 -0300

    Field centric drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TechMakerTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TechMakerTeleop.java
@@ -4,7 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.subsystems.Elevator;
+import org.firstinspires.ftc.teamcode.drive.subsystems.FieldOriented;
+import org.firstinspires.ftc.teamcode.drive.subsystems.Launcher;
+import org.firstinspires.ftc.teamcode.drive.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.drive.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.drive.subsystems.SampleMecanumDrive;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 
 @TeleOp(group="drive")
@@ -14,58 +21,60 @@ public class TechMakerTeleop extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         //SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        //drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        //Intake intake = new Intake(hardwareMap);
+       // drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        FieldOriented fieldOriented = new FieldOriented();
+        fieldOriented.runOpMode();
+
+        Intake intake = new Intake(hardwareMap);
 
         Elevator elevator = new Elevator(hardwareMap);
 
         //Climber climber = new Climber(hardwareMap);
 
-        //Launcher launcher = new Launcher(hardwareMap);
+        Launcher launcher = new Launcher(hardwareMap);
 
         Claw claw = new Claw(hardwareMap);
 
         waitForStart();
-        elevator.resetArmEncoders();
+       elevator.resetArmEncoders();
 
 
 
         while (!isStopRequested()) {
-
-            /*
-            mecanumVelocity = (5500 - arm.getCurrentArmPosition()) / 5600.0;
+            /*mecanumVelocity = (5500 - 0) / 5600.0;
             drive.setWeightedDrivePower(
                     new Pose2d(-gamepad1.left_stick_y * mecanumVelocity, -gamepad1.left_stick_x * mecanumVelocity, -gamepad1.right_stick_x * mecanumVelocity));
+
+
              */
-            //Arm
-            if(gamepad1.left_trigger>0.75){
+            // Arm
+            while (gamepad2.left_trigger>0.5){
                 elevator.teste();
             }
-            if (gamepad1.right_trigger>0.73) {
+            while (gamepad2.right_trigger>0.5){
                 elevator.voltaTeste();
             }
-           //arm.task();
 
-            // claw
-            if (gamepad1.right_bumper) {
-                claw.open();
+
+            //Launcher
+            while (gamepad2.a){
+                launcher.activate();
             }
-            else if(gamepad1.left_bumper){
+            launcher.stop();
+
+            //intake
+            if(gamepad2.left_bumper){
+                intake.activate();
+                claw.open();
+            }else if(gamepad2.right_bumper){
+                intake.close();
+            } else {
+                intake.stop();
                 claw.close();
             }
 
-            /*
-            //Launcher
-            if (gamepad1.b){
-                launcher.activate();
-            }else{launcher.stop();}
-
-            //ResetEncoders
-            if (gamepad1.back){
-                arm.resetArmEncoders();
-            }
-             */
         }
     }
 }
